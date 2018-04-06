4b7e9189d648799693bcc8c73171b9cf35d96b4a
Ryan Udelhoven <Ryan@Udelhoven.net>
2018-04-06 08:50:22 -0500

    added routers to match services

---
+++ server/routes/guest.router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const pool = require('../modules/pool.js');
+const router = express.Router();
+
+
+
+  module.exports = router;
--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const sessionConfig = require('./modules/session-middleware');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const adminRouter = require('./routes/admin.router');
+const guestRouter = require('./routes/guest.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -21,6 +23,8 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/admin', adminRouter);
+app.use('/guest', guestRouter);
 
 // Serve static files
 app.use(express.static('server/public'));
