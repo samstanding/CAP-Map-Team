0d489701aada46ec1e849140557102e4e77f5a84
Elizabeth Wong <lizzwong@gmail.com>
2018-04-09 13:14:56 -0500

    guest event view getting from database

--- server/public/scripts/controllers/guest_controllers/guest_events.controller.js
+++ server/public/scripts/controllers/guest_controllers/guest_events.controller.js
@@ -1,7 +1,13 @@
-capApp.controller('GuestEventsController', ['UserService', 'GuestService', function (UserService, GuestService) {
+capApp.controller('GuestEventsController', ['UserService', 'GuestService','AdminService', function (UserService, GuestService, AdminService) {
     console.log('GuestEventsController created');
     var self = this;
     self.userService = UserService;
     self.guestService = GuestService;
+    self.adminService = AdminService;
+    self.getEvents = AdminService.getEvents;
+    self.locations = AdminService.locations;
+
+    self.getEvents();
+
 
 }]);
