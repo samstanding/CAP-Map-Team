4e70f3cf62d6be5af52f462775d8acd4b9248779
Ryan Udelhoven <Ryan@Udelhoven.net>
2018-04-10 10:44:03 -0500

    minor router changes, basic syntax cleanup

--- server/routes/information.router.js
+++ server/routes/information.router.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post('/post', (req,res)=>{
 const guideline = req.body;
 const sqlText = `INSERT INTO information (description, category)
-VALUES ($1, $2)`;
+VALUES ($1, $2);`;
 pool.query(sqlText, [guideline.description, guideline.category])
     .then(function(result){
         console.log('Information added', result);
@@ -18,7 +18,7 @@ pool.query(sqlText, [guideline.description, guideline.category])
 })
 
 router.get('/get', (req,res)=>{
-    pool.query(`SELECT * FROM information ORDER BY id`)
+    pool.query(`SELECT * FROM information ORDER BY id;`)
     .then(function(result){
         res.send(result.rows);
     })
@@ -29,7 +29,7 @@ router.get('/get', (req,res)=>{
 
 router.put('/edit/:id', (req,res)=>{
     const guideline = req.body;
-    const sqlText = `UPDATE information SET description = $1, category = $2 WHERE id = $3`;
+    const sqlText = `UPDATE information SET description = $1, category = $2 WHERE id = $3;`;
     pool.query(sqlText, [guideline.description, guideline.category, guideline.id])
     .then(function(result){
         console.log('Guideline updated', result);
@@ -44,7 +44,7 @@ router.put('/edit/:id', (req,res)=>{
 router.delete('/delete/:id', (req, res) => {
     // if (req.isAuthenticated()) {
     const id = req.params
-    pool.query(`delete from information where id = $1;`, [req.params.id])
+    pool.query(`delete from information where id = $1;;`, [req.params.id])
     // } else {
     //     res.sendStatus(403);
     // }
--- server/routes/map.router.js
+++ server/routes/map.router.js
@@ -30,18 +30,22 @@ router.get('/artifact/:id', (req, res) => {
 });
 
 router.delete('/join/delete/:id', (req, res) => {
-    let id = req.params.id;
-    pool.query('DELETE * FROM map_artifact_join where id = $1', [id])
+    // if (req.isAuthenticated()) {
+        let id = req.params.id;
+        pool.query('DELETE * FROM map_artifact_join where id = $1;', [id])
         .then(function (result) {
             res.send(result.rows);
         }).catch(function (error) {
             res.sendStatus(500);
         })
+    // } else {
+    //     res.sendStatus(403);
+    // }
 });
 
 router.post('/join/insert', (req, res) => {
     // if (req.isAuthenticated()) {
-    pool.query('INSERT INTO map_artifact_join (artifact_id, location_id, main_photo) VALUES ($1, $2, $3)',
+        pool.query('INSERT INTO map_artifact_join (artifact_id, location_id, main_photo) VALUES ($1, $2, $3);',
         [req.body.artifact_id, req.body.location_id, req.body.main_photo], (err, result) => {
             if (err) {
                 console.log("Error inserting data: ", err);
@@ -50,6 +54,23 @@ router.post('/join/insert', (req, res) => {
                 res.sendStatus(201);
             }
         });
+    // } else {
+    //     res.sendStatus(403);
+    // }
 });
+
+router.post('/location/post', (req, res) => {
+    // if (req.isAuthenticated()) {
+        let location = req.body;
+        pool.query('INSERT INTO map (location_name, lat, long, reveal_type);', [location.location_name, location.lat, location.long, location.reveal_type])
+        .then(function (result) {
+            res.sendStatus(201);
+        }).catch(function (error) {
+            res.sendStatus(500);
+        })
+    // } else {
+    //     res.sendStatus(403);
+    // }
+})
 
 module.exports = router;
