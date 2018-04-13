44cb3c2a794327c6e324c89824440020237c1363
Sam Standing <samzstanding@gmail.com>
2018-04-13 14:36:10 -0500

    deleted extraneous add location files

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -57,25 +57,6 @@ capApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $
         }
       }
     })
-    .when('/admin/namelocation/', {
-      templateUrl: '/views/admin/name_location.html',
-      controller: 'NameLocationController as vm',
-      resolve: {
-        getuser: function (UserService) {
-          return UserService.getuser();
-        }
-      }
-    })
-    .when('/admin/locationpreview', {
-      templateUrl: '/views/admin/location_preview.html',
-      controller: 'LocationPreviewController as vm',
-      // resolve: {
-      //   getuser: function (UserService) {
-      //     return UserService.getuser();
-      //   }
-      // }
-
-    })
     .when('/admin/editlocation', {
       templateUrl: '/views/admin/edit_location.html',
       controller: 'EditLocationController as vm',
--- server/public/scripts/controllers/admin_controllers/add_location.controller.js
+++ server/public/scripts/controllers/admin_controllers/add_location.controller.js
@@ -1,17 +1,61 @@
-capApp.controller('AddLocationController', ['UserService', 'AdminService', '$location',  function (UserService, AdminService, $location) {
-    console.log('AddLocationController created');
-    let self = this;
+capApp.controller('AddLocationController', ['UserService', 'AdminService', function (UserService, AdminService) {
     
-    //don't think i need these since I'm not sending anything to the db
+    var self = this;
+    // don't think I need this
     // self.userService = UserService;
     self.adminService = AdminService;
-   
-    //get findlocation function from adminservice
-    self.findLocation = AdminService.findLocation;
 
-    //function to route the page on click to the name location
-   self.movePage = () => {
-    $location.url('/admin/namelocation/');
-   }
- 
+    self.locations = AdminService.locations;
+
+    // nor do i need any of these
+    // self.getAllLocations = AdminService.getAllLocations;
+    // self.getAllLocations();
+
+    console.log(self.locations.newLocation.coords);
+    
+    self.initMap = () => {
+        geocoder = new google.maps.Geocoder();
+       self.map = new google.maps.Map(document.getElementById('map'), {
+            center : {
+                lat:44.80457827564791,
+                lng: -93.15323458993169
+            }, 
+            zoom: 18,
+            streetViewControl: false,
+        })
+        let infowindow = new google.maps.InfoWindow({
+            content: self.locations.newLocation.name
+        });
+
+        let marker = new google.maps.Marker({
+            position: new google.maps.LatLng(44.80457827564791,-93.15323458993169),
+            map: self.map,
+            title: self.locations.newLocation.name,
+            draggable: true,
+            animation: google.maps.Animation.DROP
+        })
+
+
+        google.maps.event.addListener(marker, 'dragstart', function () {
+            console.log('drag start');
+            
+        })
+
+        google.maps.event.addListener(marker, 'drag', function () {
+            console.log('dragging');
+            
+        })
+
+        google.maps.event.addListener(marker, 'dragend', function () {
+            console.log('dragend');
+            self.locations.newLocation.lat =marker.getPosition().lat();
+            self.locations.newLocation.long =marker.getPosition().lng();
+            console.log(self.locations.newLocation);
+            
+            
+        })
+    }
+
+   
+    self.initMap();
 }]);
