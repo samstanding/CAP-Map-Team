5c9c3b21f7f3bad526a007a80982eb160da2507d
Sam Standing <samzstanding@gmail.com>
2018-04-13 16:40:58 -0500

    added overlay to to add location view, working out kinks

--- server/public/scripts/controllers/admin_controllers/add_location.controller.js
+++ server/public/scripts/controllers/admin_controllers/add_location.controller.js
@@ -1,5 +1,4 @@
 capApp.controller('AddLocationController', ['UserService', 'AdminService', function (UserService, AdminService) {
-    
     var self = this;
     // don't think I need this
     // self.userService = UserService;
@@ -9,29 +8,29 @@ capApp.controller('AddLocationController', ['UserService', 'AdminService', funct
 
     self.locations = AdminService.locations;
 
-    // nor do i need any of these
-    // self.getAllLocations = AdminService.getAllLocations;
-    // self.getAllLocations();
-
-    console.log(self.locations.newLocation.coords);
+   let overlay;
+   CaponiOverlay.prototype = new google.maps.OverlayView();
     
     self.initMap = () => {
         
-       self.map = new google.maps.Map(document.getElementById('map'), {
+       let map = new google.maps.Map(document.getElementById('map'), {
             center : {
-                lat:44.80457827564791,
-                lng: -93.15323458993169
+                lat: 44.80526000, 
+                lng: -93.15375000
             }, 
-            zoom: 18,
-            streetViewControl: false,
+            zoom: 20,
+            mapTypeId: 'satellite'
         })
-        let infowindow = new google.maps.InfoWindow({
-            content: self.locations.newLocation.name
-        });
+       
+        let bounds = new google.maps.LatLngBounds(
+            new google.maps.LatLng(44.8048, -93.1577000),
+            new google.maps.LatLng(44.8085000, -93.1457900));
+    
+        let srcImage = '../../styles/northMap.png';
 
         let marker = new google.maps.Marker({
             position: new google.maps.LatLng(44.80457827564791,-93.15323458993169),
-            map: self.map,
+            map: map,
             title: self.locations.newLocation.name,
             draggable: true,
             animation: google.maps.Animation.DROP
@@ -54,6 +53,78 @@ capApp.controller('AddLocationController', ['UserService', 'AdminService', funct
             self.locations.newLocation.long =marker.getPosition().lng();
             console.log(self.locations.newLocation);      
         })
+        overlay = new CaponiOverlay(bounds, srcImage, map);
     }   
+      /** @constructor */
+      function CaponiOverlay(bounds, image, map) {
+
+        // Initialize all properties.
+        this.bounds_ = bounds;
+        this.image_ = image;
+        this.map_ = map;
+  
+        // Define a property to hold the image's div. We'll
+        // actually create this div upon receipt of the onAdd()
+        // method so we'll leave it null for now.
+        this.div_ = null;
+  
+        // Explicitly call setMap on this overlay.
+        this.setMap(map);
+      }
+  
+      /**
+       * onAdd is called when the map's panes are ready and the overlay has been
+       * added to the map.
+       */
+      CaponiOverlay.prototype.onAdd = function () {
+  
+        var div = document.createElement('div');
+        div.style.borderStyle = 'none';
+        div.style.borderWidth = '0px';
+        div.style.position = 'absolute';
+  
+        // Create the img element and attach it to the div.
+        var img = document.createElement('img');
+        img.src = this.image_;
+        img.style.width = '100%';
+        img.style.height = '100%';
+        img.style.position = 'absolute';
+        img.style.opacity = '.9';
+        div.appendChild(img);
+  
+        this.div_ = div;
+  
+        // Add the element to the "overlayLayer" pane.
+        var panes = this.getPanes();
+        panes.overlayLayer.appendChild(div);
+      };
+  
+      CaponiOverlay.prototype.draw = function () {
+  
+        // We use the south-west and north-east
+        // coordinates of the overlay to peg it to the correct position and size.
+        // To do this, we need to retrieve the projection from the overlay.
+        var overlayProjection = this.getProjection();
+  
+        // Retrieve the south-west and north-east coordinates of this overlay
+        // in LatLngs and convert them to pixel coordinates.
+        // We'll use these coordinates to resize the div.
+        var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
+        var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
+  
+        // Resize the image's div to fit the indicated dimensions.
+        var div = this.div_;
+        div.style.left = sw.x + 'px';
+        div.style.top = ne.y + 'px';
+        div.style.width = (ne.x - sw.x) + 'px';
+        div.style.height = (sw.y - ne.y) + 'px';
+      };
+  
+      // The onRemove() method will be called automatically from the API if
+      // we ever set the overlay's map property to 'null'.
+      CaponiOverlay.prototype.onRemove = function () {
+        this.div_.parentNode.removeChild(this.div_);
+        this.div_ = null;
+      };
     self.initMap();
 }]);
